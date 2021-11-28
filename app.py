d5b9c78d5e0cb23965bfa4f2a0293b5e1e29ddd6
ldlasso12 <laurad.lasso.a@gmail.com>
2021-11-28 01:10:07 -0500

    Font test

--- app.py
+++ app.py
@@ -88,6 +88,25 @@ RTC_CONFIGURATION = RTCConfiguration(
 
 
 def main():
+    st.markdown(     """
+        <style>
+        @font-face {
+        font-family: 'Tangerine';
+        font-style: normal;
+        font-weight: 400;
+        src: url(https://fonts.gstatic.com/s/tangerine/v12/IurY6Y5j_oScZZow4VOxCZZM.woff2) format('woff2');
+        unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+        }
+
+            html, body, [class*="css"]  {
+            font-family: 'Tangerine';
+            font-size: 48px;
+            }
+            </style>
+
+            """,
+                unsafe_allow_html=True,
+            )
     st.header("Trascender Global !")
     header_html = "<img src='data:image/png;base64,{}' class='img-fluid'>".format(
     img_to_bytes("HOR-Header.png"))
